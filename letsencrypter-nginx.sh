9da13020f6fa4830f9ed01961cdaed6fc4d9a4cf
laurent <laurent.cetinsoy@gmail.com>
2016-03-30 02:24:37 +0200

    added autorenewal cronjob

--- letsencrypter-nginx.sh
+++ letsencrypter-nginx.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-if ( ! type letsencrypt-auto ); then
+source ./auto-renewal.sh
+
+install-letsencrypt () {
+
+  $installationPath = $0
+
+  git clone https://github.com/letsencrypt/letsencrypt $installationPath
+
+}
+
+if ( ! type letsencrypt-auto > /dev/null 2>&1 ); then
 
   echo "letsencrypt-auto was not found"
-  echo "provide a path to it. Leave blank for automatic installation
-  in ~/letsencrypt"
+  echo "do you want to it to be installed automatically y/n?"
+  echo "it will be installed in ~/letsencrypt"
 
+  read install
+  if [ "y" = "$install" ]; then
+
+    git clone https://github.com/letsencrypt/letsencrypt ~/letsencrypt
+    cd ~/letsencrypt
+
+  else
+
+    echo "aborting. Install letsencrypt and rerun the script"
+    exit 0
+  fi
 fi
 
 echo "domain to encrypt"
@@ -14,7 +35,7 @@ read domain
 echo "document root"
 read documentRoot
 
-echo "nginx site vhost path"
+echo "nginx site vhost absolute path"
 read nginxVhost
 
 #for lets encrypt process
@@ -33,7 +54,7 @@ echo "server {
 
          listen 443 ssl;
          listen [::]:443 ssl default_server;
-
+         server_name $domain
          ssl on;
          ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
          ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
@@ -41,17 +62,20 @@ echo "server {
          root $documentRoot;
          # Add index.php to the list if you are using PHP
          index index.html index.htm index.nginx-debian.html;
-         location / {
-
-         }
-
 
 
 }" >> $nginxVhost
 
+sudo nginx -t && sudo service nginx reload || echo "an error occured"
 
+echo "do you want to add certificate auto renewal with a cron job  ? y/n"
 
-sudo nginx -t && sudo service nginx reload
+read autoRenew
+if [ "y" = "$autoRenew" ]; then
 
+  add-auto-renewal-cronjob
+fi
 
-echo "end"
+echo "Done."
+echo "Please check that your nginx config file is the way you want and edit it accordingly"
+echo "Not everything is automated yet"
